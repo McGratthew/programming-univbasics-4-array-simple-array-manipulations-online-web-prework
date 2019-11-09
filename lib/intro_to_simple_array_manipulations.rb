4aa3d3bff6fcfef7679e9f75b7e6b816c26aa42e
Matt Miller <mbmiller0507@yahoo.com>
2019-11-09 20:09:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,6 +28,6 @@ def using_concat(array, more_favs)
   array.concat(more_favs)
 end
   
-def using_insert(array)
-  array.insert(3, "Python")
+def using_insert(array, element)
+  array.insert(3, element)
 end
